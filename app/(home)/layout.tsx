16722bfd05f20b64908099961167aa0053826c65
Alberto Chen <xchao0213@gmail.com>
2025-11-05 09:38:26 +0800

    Revert "docs default"
    This reverts commit 06ffdb6fec797bbeb779737b613b103f0e049046.

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -2,7 +2,7 @@ import { source } from '@/lib/source';
 import { DocsLayout } from 'fumadocs-ui/layouts/docs';
 import { baseOptions } from '@/lib/layout.shared';
 
-export default function Layout({ children }: LayoutProps<'/'>) {
+export default function Layout({ children }: LayoutProps<'/docs'>) {
   return (
     <DocsLayout tree={source.pageTree} {...baseOptions()}>
       {children}
